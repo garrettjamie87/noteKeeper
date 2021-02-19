4665742dc90b915ba7666d2efdea5a8796b1da2a
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-19 14:23:38 +0100

    change file names to conform with industry standard. Map over index.js file to render <Note />

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./../notes"
+
+function cards(el){
+  return  <Note
+    key={el.key}
+    title={el.title}
+    content={el.content}
+    />
+    }
 
 function App() {
   return (
     <>
       <Header />
       <Footer />
-      <Note />
+      {notes.map(cards)}
     </>
   );
 }
